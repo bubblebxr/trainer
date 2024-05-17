fe9ab0157a67e79451238c9a6878b1806d782325
Candy <candyLyl687423@126.com>
2024-05-17 08:20:43 +0800

    add Food v1

---
+++ backend/src/main/java/com/example/se_project/controller/FoodController.java
@@ -0,0 +1,16 @@
+package com.example.se_project.controller;
+
+import com.example.se_project.entity.Food;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class FoodController {
+    List<Food> getAllFood(@RequestBody Map<String, String> requestMap){
+
+    }
+}

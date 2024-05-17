a9964361c8edcef545e511fa88bfad599f4e0bf1
Candy <candyLyl687423@126.com>
2024-05-17 08:24:32 +0800

    add Food v1

--- backend/src/main/java/com/example/se_project/entity/Station.java
+++ backend/src/main/java/com/example/se_project/entity/Station.java
@@ -1,7 +1,13 @@
 package com.example.se_project.entity;
 
+import java.util.Date;
+
 public class Station {
     private String stationName;
+
+    private Date arriveTime;
+
+    private long duration;
 
     public String getStationName() {
         return stationName;
@@ -9,5 +15,21 @@ public class Station {
 
     public void setStationName(String stationName) {
         this.stationName = stationName;
+    }
+
+    public Date getArriveTime() {
+        return arriveTime;
+    }
+
+    public void setArriveTime(Date arriveTime) {
+        this.arriveTime = arriveTime;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
     }
 }

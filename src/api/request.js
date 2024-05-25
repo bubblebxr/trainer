fb4840932880a34855c32a36f423d09f2817c391
buxinran <141600249+buxinran@users.noreply.github.com>
2024-05-25 18:51:15 +0800

    登录验证，更新密码

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const api = axios.create(
   {
-    baseURL: "http://127.0.0.1:4523/m1/4458630-0-default", //这里配置的是后端服务提供的接口
+    baseURL: "http://localhost:9090", //这里配置的是后端服务提供的接口
   }
 );
 export default api;

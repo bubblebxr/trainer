d336629cf1819c3aa203d552c05ff4c802f10f00
WZQ <13875445+bush04@user.noreply.gitee.com>
2024-05-25 15:31:25 +0800

    发送邮件的接口EmailService（改为SimpleMail）

--- backend/src/main/java/com/example/se_project/service/Impl/EmailService.java
+++ backend/src/main/java/com/example/se_project/service/Impl/EmailService.java
@@ -1,15 +1,10 @@
 package com.example.se_project.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.core.io.FileSystemResource;
-
-import java.io.File;
 
 @Service
 public class EmailService {
@@ -17,31 +12,11 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendHtmlEmail(String to, String subject, String htmlBody) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(htmlBody, true);
-        helper.setFrom("your-email@example.com");
-
-        mailSender.send(message);
-    }
-
-    public void sendEmailWithAttachment(String to, String subject, String text, String pathToAttachment) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(text);
-        helper.setFrom("your-email@example.com");
-
-        // 添加附件
-        FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
-        helper.addAttachment(file.getFilename(), file);
-
+    public void sendSimpleMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
         mailSender.send(message);
     }
 }

f23f730193835cd7aa9b4ccea53a244a532554de
Candy <candyLyl687423@126.com>
2024-08-21 09:39:08 +0800

    add user test

--- backend/src/main/java/com/buaa/werwertrip/controller/UserController.java
+++ backend/src/main/java/com/buaa/werwertrip/controller/UserController.java
@@ -40,7 +40,6 @@ public class UserController {
         int time = Integer.parseInt(registerMap.get("time").toString());
         Map<String, Object> map = new HashMap<>();
 
-        //System.out.println(id);
         // 校验身份证号
         if (!id.matches("^[1-9]\\d{5}(18|19|([23]\\d))\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$")) {
             map.put("result", false);
--- backend/src/test/java/com/buaa/werwertrip/controller/UserControllerTest.java
+++ backend/src/test/java/com/buaa/werwertrip/controller/UserControllerTest.java
@@ -58,24 +58,47 @@ class UserControllerTest {
 
         // 用例5：成功
         Map<String, Object> userDetails = new HashMap<>();
-        userDetails.put("id", "120102200001012356");
+        userDetails.put("id", "90123419860118123X");
         userDetails.put("name", "陆");
         userDetails.put("password", "123456AAA");
-        userDetails.put("email", "123456@qq.com");
+        userDetails.put("email", "3181577132@qq.com");
         userDetails.put("time","2");
         Assertions.assertTrue((Boolean)(userController.userRegister(userDetails).get("result")));
     }
 
     @Test
     void showUserInfo() {
+        Map<String,Object> userInfo = userController.showUserInfo("102102200802100328");
+        Assertions.assertNotNull(userInfo);
     }
 
     @Test
     void userLogin() {
+        // 用例1：未注册
+        Map<String, Object> userLoginMap1 = new HashMap<>();
+        userLoginMap1.put("id", "100102200001012345");
+        userLoginMap1.put("password", "123456AAA");
+        Assertions.assertFalse((Boolean)(userController.userLogin(userLoginMap1).get("result")));
+
+        // 用例2：密码错误
+        Map<String, Object> userLoginMap2 = new HashMap<>();
+        userLoginMap2.put("id", "120102200001012345");
+        userLoginMap2.put("password", "123456AAA");
+        Assertions.assertFalse((Boolean)(userController.userLogin(userLoginMap2).get("result")));
+
+        // 用例3：成功登录
+        Map<String, Object> userLoginMap3 = new HashMap<>();
+        userLoginMap3.put("id", "89012319991209145X");
+        userLoginMap3.put("password", "123456AAA");
+        Assertions.assertTrue((Boolean)(userController.userLogin(userLoginMap3).get("result")));
     }
 
     @Test
     void updatePassword() {
+        Map<String, Object> updatePasswordMap = new HashMap<>();
+        updatePasswordMap.put("id","90123419860118123X");
+        updatePasswordMap.put("newpassword","123456BBB");
+        Assertions.assertTrue((Boolean)(userController.updatePassword(updatePasswordMap).get("result")));
     }
 
     @Test
